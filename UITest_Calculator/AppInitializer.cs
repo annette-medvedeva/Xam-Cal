93f1edb58e108e25e754975d149fc1e346834d7e
annette-medvedeva <63592046+annette-medvedeva@users.noreply.github.com>
2020-08-14 17:20:42 +0300

    Add files via upload

---
+++ UITest_Calculator/AppInitializer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.UITest;
+
+namespace UITest_Calculator
+{
+    public static class AppInitializer
+    {
+        static IApp _app;
+        private static string _simID = null;
+        private static string _apkPath = @"C:\Users\Anna\source\repos\CalculXAMARINE\CalculXAMARINE\bin\Release\com.companyname.calculxamarine.apk";
+
+        public static IApp App
+        {
+            get { return _app; }       
+        }
+
+        public static void StartApp()
+        {
+            var configuratorAndroid = ConfigureApp.Android.EnableLocalScreenshots();
+            if (_simID !=null)
+            {
+                configuratorAndroid.DeviceSerial(_simID);
+            }
+            _app = configuratorAndroid.ApkFile(_apkPath).StartApp();
+        }
+
+    }
+}
